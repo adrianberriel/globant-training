06fd78116b0ed2af54bcc87497d83e2275a78cc6
Adrian Berriel <berriel@gmail.com>
2014-03-08 01:48:25 -0200

    Version 2 entregada

--- modulo_2/app.js
+++ modulo_2/app.js
@@ -1,86 +1,130 @@
 //Datos de prueba
 var empleados = {
-    '1001' : {
-        nombre: 'Juan',
-        salario: 23400 
-    },
-    '1002' : { 
-        nombre: 'Pedro',
-        salario: 32200
-    },
-    '1003' : { 
-        nombre: 'Martin',
-        salario: 39430 
+    'empleados' : {
+        'empleado' : [
+        {
+            id: 1001,
+            nombre: 'Juan',
+            salario: 23400 
+        },
+        { 
+            id: 1002,
+            nombre: 'Pedro',
+            salario: 32200
+        },
+        { 
+            id: 1003,
+            nombre: 'Martin',
+            salario: 39430 
+        }
+        ]
     }
-}
+};
 
 var express = require('express');
-var app = express();
 var js2xmlparser = require("js2xmlparser");
+var xml2js = require('xml2js');
 
+var app = express();
 
-app.get('/empleados', function(req, res){
-    switch (req.get('accept')) {
+app.use(function(req, res, next) {
+    req.rawBody = '';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk) {
+        req.rawBody += chunk;
+    });
+    req.on('end', function() {
+        next();
+    });
+});
+
+function rawBody2json(req) {
+    var body = req.rawBody;
+    var datosJson;
+    var contentType = req.get('Content-type');
+    switch (contentType) {
         case 'application/json':
-            res.send(empleados);
+            datosJson = JSON.parse(body);
             break;
         case 'application/xml':
         case 'text/xml':
-            res.set('Content-Type', 'text/xml');
-            res.send(js2xmlparser('empleados', empleados));
-            break;
-        default:
-            res.send(406, { 'error' : 'campo accept invalido' });
+            xml2js.parseString(body, function (err, result) {
+                datosJson = result;
+            });
             break;
     }
+    return datosJson;
+}
+
+function empleadosConFormato(req, emp) {
+    var accept = req.get('accept');
+    if (accept == 'application/xml' || accept == 'text/xml') {
+        return js2xmlparser('empleados', emp);
+    }
+    return emp;
+}
+
+app.put('/actualizar', function(req, res){
+    var empAct = rawBody2json(req);
+    empAct = empAct['empleados']['empleado'];
+    var empViejos = empleados['empleados']['empleado'];
+    for (var emp in empAct) {
+        var empId = empAct[emp]['id'];
+        var actual = empAct[emp];
+        for (var viejo in empViejos) {
+            var empIdViejo = empViejos[viejo]['id'];
+            var empViejo = empViejos[viejo];
+            if (empId == empIdViejo) {
+                empViejos[viejo] = actual;
+            }
+        }
+    }
+    res.send(empleadosConFormato(req, empleados));
+});
+
+app.post('/crear', function(req, res){
+    var empNuevos = rawBody2json(req);
+    empNuevos = empNuevos['empleados']['empleado'];
+    var empViejos = empleados['empleados']['empleado'];
+    empleados['empleados']['empleado'] = empViejos.concat(empNuevos);
+
+    res.send(empleadosConFormato(req, empleados));
+});
+
+app.delete('/eliminar/:id', function(req, res){
+    var id = req.params.id;
+    var listaEmpleados = empleados['empleados']['empleado'];
+    //var empleado;
+    for (var emp in listaEmpleados) {
+        var empId = listaEmpleados[emp]['id'];
+        if (id == empId) {
+            //empleado = listaEmpleados[emp];
+            //delete empleado;
+            delete empleados[listaEmpleados[emp]];
+            res.send(empleadosConFormato(req, empleados));
+        }
+    }
+    res.send(406, 'Empleado no encontrado');
+});
+
+app.get('/empleados', function(req, res){
+    var emp = empleados;
+    res.send(empleadosConFormato(req, emp));
 });
 
 app.get('/empleado/:id', function(req, res){
     var id = req.params.id;
-    var empleado = empleados[id];
-    switch (req.get('accept')) {
-        case 'application/json':
-            if (empleado !== undefined) {
-                res.send(empleado);
-            }
-            break;
-        case 'application/xml':
-        case 'text/xml':
-            res.set('Content-Type', 'text/xml');
-            res.send(js2xmlparser('empleados', empleado));
-            break;
-        default:
-            res.send(406, { 'error' : 'campo accept invalido' });
-            break;
+    var listaEmpleados = empleados['empleados']['empleado'];
+    var empleado;
+    for (var emp in listaEmpleados) {
+        var empId = listaEmpleados[emp]['id'];
+        if (id == empId) {
+            empleado = listaEmpleados[emp];
+            res.send(empleadosConFormato(req, empleado));
+        }
     }
-    res.send(404, { 'error' : 'empleado no encontrado' });
+    res.send(406, 'Empleado no encontrado');
 });
-
-app.post('/crear', function(req, res){
-    var empleadosNuevos;
-    switch (req.get('accept')) {
-        case 'application/json':
-            if (req.get('content-type') === 'application/json') {
-                empleadosNuevos = req.body;
-                for (var e in empleadosNuevos) {
-                    console.log(e);
-                    empleados[e] = { 'nombre' : e['nombre'], 'salario' : e['salarioi'] }
-                }
-            }
-            res.send(empleados);
-            break;
-        case 'application/xml':
-        case 'text/xml':
-            res.set('Content-Type', 'text/xml');
-            res.send(js2xmlparser('empleados', empleado));
-            break;
-        default:
-            res.send(406, { 'error' : 'campo accept invalido' });
-            break;
-    }
-    res.send(404, { 'error' : 'empleado no encontrado' });
-});
-
 
 app.listen(3000);
 console.log('Listening on port 3000...');
