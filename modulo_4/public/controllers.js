7d29d42dc7ff1df0776661656b952872a43643fa
Adrian Berriel <berriel@gmail.com>
2014-03-29 05:33:12 -0300

    Small changes

--- modulo_4/public/controllers.js
+++ modulo_4/public/controllers.js
@@ -1,13 +1,26 @@
 var controllers = angular.module('app.controllers',[]);
 
-controllers.controller('ListController', function($scope, ListService){
+//var employeeList;
+
+controllers.controller('ListController', function($scope, ListService, $routeParams){
     $scope.employees = ListService.getEmployees();
+    //employeeList = angular.copy($scope.employees);
+    //alert('ListController');
 });
 
 controllers.controller('EmployeeController', function($scope, EmployeeService, $routeParams){
     $scope.employee = EmployeeService.getEmployee($routeParams.employeeId);
-    
-    alert($scope.employee.name);
+    //alert(employeeList[0]);
+
+
+    //$scope.employee = { id: 1, name: "asdasdas", salary: 200012 };
+    //$scope.employee = employeeList[$routeParams.employeeId];
+    //$scope.employee = angular.copy(employee);
+    //$scope.employee = angular.copy($scope.employees);
+
+    //promise.then(function(data) {
+        //$scope.employee = data;
+    //});
 
     $scope.updateEmployee = function() {
         //$scope.updatedEmployee = angular.copy($scope.employee);
--- modulo_4/public/services.js
+++ modulo_4/public/services.js
@@ -1,50 +1,69 @@
 var services = angular.module('app.services',[]);
 
-services.service('ListService',function($http, $q){
-	var EmpleadosService = {};
-	EmpleadosService.getEmployees = function(){
-		var deferred = $q.defer();
-		$http.get("/employees", {"headers" : {"Accept" : "application/json"}})
-		.success(function(data){
-			deferred.resolve(data);
-		})
-		.error(function(){
-			deferred.reject("An error occured while fetching status");
-		});
-		
-		return deferred.promise;
-	}
-	
-	return EmpleadosService;
-});
-
-services.service('EmployeeService',function($http, $q){
-    this.getEmployee = function(id){
+services.service('ListService', function($http, $q){
+    var ListService = {};
+    ListService.getEmployees = function(){
         var deferred = $q.defer();
-        $http.get("/employees/" + id, {"headers" : {"Accept" : "application/json"}})
-        .success(function(data){
-            deferred.resolve(data);
-        })
-        .error(function(){
-            deferred.reject("An error occured while fetching status");
-        });
+        $http.get("/employees", {"headers" : {"Accept" : "application/json"}})
+            .success(function(data){
+                deferred.resolve(data);
+            })
+            .error(function(){
+                deferred.reject("An error occured while fetching status");
+            });
+
         return deferred.promise;
     }
-
-    this.updateEmployee = function(employee){
-        alert('Service: employee.id ' + employee.id + 'employee.name ' + employee.name);
-        var deferred = $q.defer();
-        $http.put("/employees/" + employee.id, {"body" : {"name" : "pepe", "salary" : "9000000" }})
-        .success(function(data){
-            deferred.resolve(data);
-        })
-        .error(function(){
-            deferred.reject("An error occured while fetching status");
-        });
-        
-        return deferred.promise;
-    }
+    return ListService;
 });
+
+//services.service('EmployeeService', function($http, $q){
+    //var EmployeeService = {};
+    //EmployeeService.getEmployee = function(id){
+        //var deferred = $q.defer();
+        //$http.get("/employees/" + id, {"headers" : {"Accept" : "application/json"}})
+            //.success(function(data){
+                //deferred.resolve(data);
+            //})
+            //.error(function(){
+                //deferred.reject("An error occured while fetching status");
+            //});
+
+        //return deferred.promise;
+    //}
+
+    //this.updateEmployee = function(employee){
+        //alert('Service: employee.id ' + employee.id + 'employee.name ' + employee.name);
+        //var deferred = $q.defer();
+        //$http.put("/employees/" + employee.id, {"body" : {"name" : "pepe", "salary" : "9000000" }})
+        //.success(function(data){
+            //deferred.resolve(data);
+        //})
+        //.error(function(){
+            //deferred.reject("An error occured while fetching status");
+        //});
+        //return deferred.promise;
+    //}
+    //return EmployeeService;
+//});
+
+services.factory('EmployeeService', function($http) {
+    var EmployeeService = {
+        getEmployee: function(id) {
+            // $http returns a promise, which has a then function, which also returns a promise
+            var promise = $http.get("/employees/" + id, {"headers" : {"Accept" : "application/json"}}).then(function (response) {
+                // The then function here is an opportunity to modify the response
+                console.log(response);
+                // The return value gets picked up by the then in the controller.
+                return response.data;
+            });
+            // Return the promise to the controller
+            return promise;
+        }
+    };
+    return EmployeeService;
+});
+
 
 //services.service('EmpleadoService',function($http, $q){
 	//var EmpleadoService = {};
